351c3881c2ebaec49128351c46ee11e663737d48
Fagner_0 <134349435+Fagner-Bagundes@users.noreply.github.com>
2025-06-25 23:17:06 -0300

    entendendo CommonJS

---
+++ 001-Modulos/app.js
@@ -0,0 +1,5 @@
+const modulo1 = require(`./modulo1`);
+
+// console.log(modulo1);
+
+modulo1.falaNome(`Fagner`, `ferreira`)
---
+++ 001-Modulos/modulo1.js
@@ -0,0 +1,9 @@
+const nome = `Fagner`;
+const sobrenome = `Ferreira`;
+
+let falaNome = (nome, sobrenome) => console.log(nome, ` `, sobrenome);
+
+// falanome(nome, sobrenome);
+
+exports.falaNome = falaNome;
+
