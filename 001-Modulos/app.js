ae104a5e0156ae11154ef7bb50d4881d3c3bb987
Fagner_0 <134349435+Fagner-Bagundes@users.noreply.github.com>
2025-06-26 20:23:07 -0300

    praticando promisse

--- 001-Modulos/app.js
+++ 001-Modulos/app.js
@@ -1,10 +1,16 @@
-const { Pessoa}  = require(`./modulo1`)
 const axios = require(`axios`)
-const p1 = new Pessoa(`Nalda`, `Maria`)
 
-console.log(p1);
 
-const objsPessoas = axios(`https://jsonplaceholder.typicode.com/users`)
-.then((respose) =>{console.log(respose.data);})
-.catch((e)=>{console.log(e);
-})
+// const Pessoas = axios(`https://jsonplaceholder.typicode.com/users`)
+// .then((respose)=>{console.log(respose.data);
+// }).catch((e)=>{console.log(e);
+// })
+
+async function pessoas (id) {
+    const response = await axios(`https://jsonplaceholder.typicode.com/users`)
+    const pessoas = response.data
+    console.log(pessoas[id].name);
+}
+
+
+pessoas(0)
